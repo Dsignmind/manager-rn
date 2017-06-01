1d0ed106609a4465db737c56e596f64d426c0306
dsignmind <dsignmind@gmail.com>
2017-06-01 16:48:33 -0700

    add EmployeeList, ListItem, and actions

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,17 +1,52 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { ListView } from 'react-native';
+import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
+    componentWillMount() {
+        this.props.employeesFetch();
+        this.createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({ employees }) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(employees);
+    }
+
+    renderRow(employee) {
+        return <ListItem employee={employee} />;
+    }
+
     render() {
         return (
-            <View>
-                <Text>EMployee List</Text>
-                <Text>EMployee List</Text>
-                <Text>EMployee List</Text>
-                <Text>EMployee List</Text>
-            </View>
+            <ListView
+                enableEmptySections
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
         );
     }
 }
 
-export default EmployeeList;
+const mapStateToProps = state => {
+    //using lodash we will convert our object of employees returned from firebase
+    //into an array for the listview
+    const employees = _.map(state.employees, (val, uid) => {
+        return { ...val, uid };
+        //will look like {shift: 'Monday',phone: '123-1234' name: 'S', id: '123g1234g12'}
+    });
+
+    return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
