090c8bdb3c8abc481f2611d03fc96040a5f7af2d
dsignmind <dsignmind@gmail.com>
2017-06-02 16:42:08 -0700

    add update and removal of employees

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,6 +1,7 @@
 import {
     EMPLOYEE_UPDATE,
-    EMPLOYEE_CREATE
+    EMPLOYEE_CREATE,
+    EMPLOYEE_SAVE_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -17,6 +18,8 @@ export default (state = INITIAL_STATE, action) => {
         //whose values will be determined at run time
             return { ...state, [action.payload.prop]: action.payload.value };
         case EMPLOYEE_CREATE:
+            return INITIAL_STATE;
+        case EMPLOYEE_SAVE_SUCCESS:
             return INITIAL_STATE;
         default:
             return state;
